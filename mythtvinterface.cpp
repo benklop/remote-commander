1b2562891d317eaf4027de0056f8b6a149e65d61
Ben Klopfenstein <benklop@gmail.com>
2013-11-29 14:36:55 -0700

    Make things compile

--- mythtvinterface.cpp
+++ mythtvinterface.cpp
@@ -1,6 +1,16 @@
 #include "mythtvinterface.h"
 
-MythtvInterface::MythtvInterface(QObject *parent) :
-    DeviceInterface(parent)
+MythtvInterface::MythtvInterface(Commander *commander, QString mythTvHost, QObject *parent) :
+    DeviceInterface(commander, parent)
+{
+    mythSocket = new QTcpSocket(this);
+    mythSocket->connectToHost(mythTvHost,6546);
+}
+
+void MythtvInterface::messageSend(QString message)
+{
+}
+
+void MythtvInterface::getMessage()
 {
 }
--- mythtvinterface.h
+++ mythtvinterface.h
@@ -1,16 +1,24 @@
 #ifndef MYTHTVINTERFACE_H
 #define MYTHTVINTERFACE_H
 
+#include "deviceinterface.h"
+#include <QTcpSocket>
+
 class MythtvInterface : public DeviceInterface
 {
     Q_OBJECT
 public:
-    explicit MythtvInterface(QObject *parent = 0);
-    
+    explicit MythtvInterface(Commander *commander, QString mythTvHost = "localhost", QObject *parent = 0);
+
 signals:
-    
+
 public slots:
-    
+    void messageSend(QString message);
+    void getMessage();
+
+private:
+    QTcpSocket *mythSocket;
+
 };
 
 #endif // MYTHTVINTERFACE_H
