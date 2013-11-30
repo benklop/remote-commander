2db70097a914d4f698502b9cb279f3c3b4608676
Ben Klopfenstein <benklop@gmail.com>
2013-11-29 23:51:29 -0700

    start implementing the Network Server

--- networkinterface.cpp
+++ networkinterface.cpp
@@ -1,6 +1,14 @@
 #include "networkinterface.h"
 
-NetworkInterface::NetworkInterface(QObject *parent) :
+//listening address and port are specified as address:port, or default to 0.0.0.0:51328
+NetworkInterface::NetworkInterface(QString address, QObject *parent) :
     DeviceInterface(parent)
 {
+    server = new QTcpServer(this);
+    QStringList addr = address.split(":");
+    if(addr.at(0) == "0.0.0.0")
+        server->listen(QHostAddress::Any,addr.at(1).toInt());
+    else
+        server->listen(QHostAddress(addr.at(0)),addr.at(1).toInt());
+
 }
--- networkinterface.h
+++ networkinterface.h
@@ -1,17 +1,20 @@
 #ifndef NETWORKINTERFACE_H
 #define NETWORKINTERFACE_H
 #include "deviceinterface.h"
+#include <QTcpServer>
 
 class NetworkInterface : public DeviceInterface
 {
     Q_OBJECT
 public:
-    explicit NetworkInterface(QObject *parent = 0);
+    explicit NetworkInterface(QString address = "0.0.0.0:51328", QObject *parent = 0);
     
 signals:
     
 public slots:
     
+public:
+    QTcpServer *server;
 };
 
 #endif // NETWORKINTERFACE_H
